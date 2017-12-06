31b148527623a99da96b76b440ed60cf5c3411d5
David Dooling <david@atomist.com>
2017-12-06 15:16:10 -0600

    Use less-than-best practices
    -   Old version of Spring Boot
    -   Use @ComponentScan
    -   Use @RequestMapping on class

--- src/main/java/com/atomist/spring/SpringRestSeedApplication.java
+++ src/main/java/com/atomist/spring/SpringRestSeedApplication.java
@@ -1,8 +1,10 @@
 package com.atomist.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@ComponentScan
 @SpringBootApplication
 public class SpringRestSeedApplication {
 
--- src/main/java/com/atomist/spring/SpringRestSeedController.java
+++ src/main/java/com/atomist/spring/SpringRestSeedController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
+@RequestMapping("/")
 class SpringRestSeedController {
 
     @RequestMapping(method = GET, path = "hello/{name}")
